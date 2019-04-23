43310c83c89d3c3b1ef6013d99d82cee53480f73
Charles Sperbeck <csperbeck@hchb.com>
2019-04-23 17:18:17 -0400

    more advanced build and default icon

--- tasks/mix/tests.ts
+++ tasks/mix/tests.ts
@@ -1,6 +1,6 @@
-import { clitask } from "./index"
-import "mocha"
-import "chai"
+import clitask from "./index"
+import * from "mocha"
+import * from "chai"
 
 describe('clitask', function() {
     it('runMain', function() {
