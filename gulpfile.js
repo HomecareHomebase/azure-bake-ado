9bf7546c672e40c14a84512e77aec38c0790ee8f
Charles Sperbeck <csperbeck@hchb.com>
2019-05-26 06:03:26 -0400

    [CHORE]Bump Version

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ function cleanCoverage() {
 }
 
 function gitAddCommit(done) {
-    return shell.task(['sudo git add --a && sudo git commit -a -m "[CHORE] Update & Publish" && sudo git push'])(done());
+    return shell.task(['sudo git add --a && sudo git commit -a -m "[CHORE] Update & Publish" && sudo git push origin master'])(done());
 }
 
 function inlineCoverageSource() {
