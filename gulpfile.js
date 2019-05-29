80201d48bfdeda95814a9685d1ea1c8404af7602
Pipeline Dream <HCHB-Pipeline-Dream@hchb.com>
2019-05-29 01:32:10 -0400

    [CHORE] Updated Build

--- gulpfile.js
+++ gulpfile.js
@@ -39,6 +39,7 @@ function gitCommit(done) {
     sudo git commit --author '` + params.buildRequestedFor + ` <` + params.buildRequestedForEmail + `>' --message "[skip ci][CHORE] Update & Publish" && 
     sudo git tag v` + vss.version + ` &&
     sudo git push origin --tags`;
+    
     console.log('Git Script: ' + gitScript);
     return shell.task(gitScript)(done());
 }
